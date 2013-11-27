08769506579e563deba5ab76f2d972946c6519a0
adarsh <adarsh@adarsh-Inspiron-N5010.(none)>
2013-11-27 20:50:33 +0530

    Few UI changes and slight code cleaning

--- gen/edu/thapar/networkapp/R.java
+++ gen/edu/thapar/networkapp/R.java
@@ -24,14 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int connectButton=0x7f080006;
+        public static final int action_settings=0x7f080008;
+        public static final int connectButton=0x7f080007;
         public static final int ipEditText=0x7f080001;
         public static final int ipTextView=0x7f080000;
         public static final int msgEditText=0x7f080002;
-        public static final int msgTextView=0x7f080004;
-        public static final int receiveMsgLayout=0x7f080005;
-        public static final int sendButton=0x7f080003;
+        public static final int msgTextView=0x7f080003;
+        public static final int receiveMsgLayout=0x7f080006;
+        public static final int scroll=0x7f080004;
+        public static final int sendButton=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -43,6 +44,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int connect_textbox=0x7f050007;
+        public static final int default_conn_status=0x7f050008;
         public static final int hello_world=0x7f050002;
         public static final int ip_hinttext=0x7f050005;
         public static final int ip_textbox=0x7f050003;
--- src/edu/thapar/networkapp/MainActivity.java
+++ src/edu/thapar/networkapp/MainActivity.java
@@ -1,3 +1,16 @@
+/*
+ *  ANDROID BASED NETWORK APPLICATION
+ * ------------------------------------ 
+ * By Adarsh Krishnan 101006007
+ * &  Ankit Goyal     101006017
+ * 
+ * For final year minor project UEC 891
+ * 
+ * github: /adarshk7/networkapp_thapar
+ * 
+ */
+
+
 package edu.thapar.networkapp;
 
 import java.io.BufferedReader;
@@ -17,6 +30,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -25,9 +39,9 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	
 	private final static String TAG = "NetApp";
-	private final static Integer PORT = 14000;
+	private final static Integer PORT = 14000;// We will use TCP port 14000
 	
-	private Socket sock;
+	private Socket sock;                      // Socket is global for global access
 	
 	private Button m_sendButton;
 	private Button m_connectButton;
@@ -35,32 +49,70 @@ public class MainActivity extends Activity {
 	private EditText m_ipEditText;
 	private EditText m_msgEditText;
 	
+	private InputMethodManager m_imm;
+	
 	private LinearLayout m_receivedMessagesLayout;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        m_sendButton = (Button) findViewById(R.id.sendButton);
+	
+	/* Method: create_and_add_textview_to_linearlayout
+	 * -----------------------------------------------
+	 * @param s: String to add in textview
+	 * @param l: linearlayout to add textview in
+	 * 
+	 * Will add a textview with string s in linear layour l
+	 * 
+	 */
+	private void create_and_add_textview_to_linearlayout(String s, LinearLayout l) {
+		TextView t = new TextView(getApplicationContext());
+		t.setText(s);
+		
+		t.setLayoutParams(new LayoutParams (
+				LayoutParams.MATCH_PARENT,
+				LayoutParams.WRAP_CONTENT));
+		l.addView(t);
+	}
+	
+	/* Method: initialize_ui_components
+	 * --------------------------------
+	 * Will initialize all UI component
+	 * for this activity.
+	 * 
+	 */
+	private void initialize_ui_components() {
+		m_sendButton = (Button) findViewById(R.id.sendButton);
         m_connectButton = (Button) findViewById(R.id.connectButton);
         
         m_ipEditText = (EditText) findViewById(R.id.ipEditText);
         m_msgEditText = (EditText) findViewById(R.id.msgEditText);
         
         m_receivedMessagesLayout = (LinearLayout) findViewById(R.id.receiveMsgLayout);
-        
-        m_connectButton.setOnClickListener(new View.OnClickListener() {			
+	
+        m_imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+	}
+	
+	/* Method: set_ui_listeners
+	 * ------------------------
+	 * Sets UI listeners.
+	 * 
+	 */
+	private void set_ui_listeners() {
+		m_connectButton.setOnClickListener(new View.OnClickListener() {			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+				// Giving focus to message editview
+				// and hiding keyboard input.
+				m_ipEditText.clearFocus();
+				m_msgEditText.requestFocus();
+				m_imm.hideSoftInputFromWindow(m_msgEditText.getWindowToken(), 0);
+				
 				new Thread() {
 					public void run() {
 						try {
 							// Create Socket address for configuring socket configuration
-							SocketAddress sockaddr = new InetSocketAddress(m_ipEditText.getText().toString(), PORT);
+							SocketAddress sockaddr = new InetSocketAddress(
+									m_ipEditText.getText().toString(), 
+									PORT);
 				            
-				            // Create socket Object
+				            // Create & assign socket Object
 				            sock = new Socket();
 				 
 				            // if timeout is reached and no response is received, it will throw socket exception
@@ -69,30 +121,34 @@ public class MainActivity extends Activity {
 				            // Initiate socket connection to server
 				            sock.connect(sockaddr, timeoutMs);
 						} catch (UnknownHostException e) {
+							create_and_add_textview_to_linearlayout(
+									"ERROR: Host Unknown", 
+									m_receivedMessagesLayout);
 				       		e.printStackTrace();
 				       	} catch (SocketTimeoutException e) {
+				       		create_and_add_textview_to_linearlayout(
+									"ERROR: Socket Timeout, retry", 
+									m_receivedMessagesLayout);
 				       		e.printStackTrace();
 				       	} catch (IOException e) {
+				       		create_and_add_textview_to_linearlayout(
+									"ERROR: IO error", 
+									m_receivedMessagesLayout);
 				       		e.printStackTrace();
 				       	}
+						Log.d(TAG, "Socket Details: " + sock.toString());
 						
-						
+						// Try catch block for receiving data
 						try {
-							Log.d(TAG, "Socket Details: " + sock.toString());
-							
             				BufferedReader rd = new BufferedReader(new InputStreamReader(sock.getInputStream()));
        		     			String str;
             				while ((str = rd.readLine()) != null) {
-            					final String str_new = str;
+            					final String str_new = "Received: " + str;
             					runOnUiThread(new Runnable() {
 									public void run() {
-										TextView newMsg = new TextView(getApplicationContext());
-	            						newMsg.setText(str_new);
-	            						
-	            						newMsg.setLayoutParams(new LayoutParams (
-	            								LayoutParams.MATCH_PARENT,
-	            								LayoutParams.WRAP_CONTENT));
-	            						m_receivedMessagesLayout.addView(newMsg);
+										create_and_add_textview_to_linearlayout(
+												str_new, 
+												m_receivedMessagesLayout);
 									}
 								});
             				}
@@ -107,14 +163,23 @@ public class MainActivity extends Activity {
         m_sendButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+				// Try catch for sending data
 				try {
         	    	// Create Buffered Writer object to write String or Byte to socket output stream
         	    	BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
-        	    	String command = m_msgEditText.getText().toString();
-        	    	wr.write(command);
-
-        	    	// Flushing the writer
+        	    	String sendstring = m_msgEditText.getText().toString();
+        	    	wr.write(sendstring);
+        	    	
+        	    	create_and_add_textview_to_linearlayout(
+							"Sent: " + sendstring, 
+							m_receivedMessagesLayout);
+        	    	Log.d(TAG, "User has sent: " + sendstring);
+        	    	
+        	    	// Hiding key soft keyboard and clearing message box.
+        	    	m_imm.hideSoftInputFromWindow(m_msgEditText.getWindowToken(), 0);
+        	    	m_msgEditText.setText("");
+        	    	
+        	    	// Flushing the writer, data will be sent now.
         	    	wr.flush();
 		
         		} catch (IOException e) {
@@ -122,6 +187,16 @@ public class MainActivity extends Activity {
         		}
 			}
 		});
+	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        initialize_ui_components();
+        
+        set_ui_listeners();
     }
 
 
